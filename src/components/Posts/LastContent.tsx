f29a27d5734a55f21b49e5db905b0ea8914aaef0
Harry Ghazni <hghazni@gmail.com>
2021-10-22 01:50:02 +0100

    Adds MUI + Card Component

--- src/components/Posts/LastContent.tsx
+++ src/components/Posts/LastContent.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { IPostsContent } from "./../../interfaces/data.interface"
 import { GatsbyImageProps } from "gatsby-image/index"
 import scss from './Posts.module.scss';
+import {Card, createTheme} from '@mui/material';
 
 import { Link } from "gatsby"
 import Img from "gatsby-image"
@@ -17,18 +18,19 @@ export default ({
     {content.map((post: IPostsContent, i: number) => {
       return isUICol ? (
         <Link to={post.slug} className={scss.post} key={i}>
-          <h3>{post.title}</h3>
-          <div></div>
-          <Img
-            className="post-image"
-            {...(post.childImageSharp as GatsbyImageProps)}
-          />
-          <p>{post.excerpt}</p>
+          <Card variant={'outlined'}>
+            <h3>{post.title}</h3>
+            <Img
+              className="post-image"
+              {...(post.childImageSharp as GatsbyImageProps)}
+            />
+            <p>{post.excerpt}</p>
 
-          <p className="post-bottom">
-            <span className="post-to-read">{post.timeToRead} min to read</span>
-            <span className="post-date">{post.date}</span>
-          </p>
+            <p className="post-bottom">
+              <span className="post-to-read">{post.timeToRead} min to read</span>
+              <span className="post-date">{post.date}</span>
+            </p>
+          </Card>
         </Link>
       ) : (
         <Link to={post.slug} className="post-row" key={i}>
