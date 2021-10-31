fb817947ecb58ebe0bee6f3c67fbd7f9df0c260d
Harry Ghazni <hghazni@gmail.com>
2021-10-31 22:53:27 +0000

    Adjust fonts and cards

--- src/components/Posts/LastContent.tsx
+++ src/components/Posts/LastContent.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import { IPostsContent } from "./../../interfaces/data.interface"
 import { GatsbyImageProps } from "gatsby-image/index"
 import scss from './Posts.module.scss';
-import {Card, createTheme} from '@mui/material';
 
 import { Link } from "gatsby"
 import Img from "gatsby-image"
@@ -18,19 +17,18 @@ export default ({
     {content.map((post: IPostsContent, i: number) => {
       return isUICol ? (
         <Link to={post.slug} className={scss.post} key={i}>
-          <Card variant={'outlined'} className={scss.card}>
+          <div className={scss.card}>
             <h3>{post.title}</h3>
             <Img
               className="post-image"
               {...(post.childImageSharp as GatsbyImageProps)}
             />
             <p>{post.excerpt}</p>
-
             <p className="post-bottom">
               <span className="post-to-read">{post.timeToRead} min to read</span>
               <span className="post-date">{post.date}</span>
             </p>
-          </Card>
+          </div>
         </Link>
       ) : (
         <Link to={post.slug} className="post-row" key={i}>
